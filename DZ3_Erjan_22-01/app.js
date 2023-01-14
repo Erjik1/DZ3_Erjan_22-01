94b7ec285ec3e6448a71b1831d320c292b77d878
Erjik1 <120021425+Erjik1@users.noreply.github.com>
2023-01-14 13:49:41 +0600

    DZ3_Erjan_22-01

---
+++ DZ3_Erjan_22-01/app.js
@@ -0,0 +1,29 @@
+// 1 часть
+// var number1 = Number(prompt('введите первое число'))
+// var number2 = Number(prompt('введите второе число'))
+// if (isNaN(number1) || isNaN(number2)) console.log('введите число!');
+// else {
+//     if (number1<number2) console.log('число' + number1 + 'меньше')
+//     else if (number1===number2) console.log('равны')
+//     else console.log('число' + number2 + 'меньше')
+// }
+// 2 часть
+// let str = prompt("Ввести первое число")
+// const strLern = (s) => {
+//     return s.length
+//
+// }
+// console.log(strLern(str));
+// 3 часть
+// let one = Number(prompt('Ввести 1 число'))
+// let two = Number(prompt('Ввести 2 число'))
+//
+// const calculator = (a, b) => {
+//     console.log(a + b + ' сложение')
+//     console.log(a - b + ' вычитание')
+//     console.log(a * b + ' умножение')
+//     console.log(a / b + ' деление')
+//
+// }
+//
+// calculator(one, two)
